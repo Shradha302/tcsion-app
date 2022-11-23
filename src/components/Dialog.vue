cf3d9967da4aac45f88afe37df8019f4888fcf0b
shradhaagnibhoj <shradha.agnibhoj@yash.com>
2022-11-23 17:52:30 +0530

    datepicker added to dialog

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -4,31 +4,42 @@
         <v-spacer></v-spacer>
         <v-dialog max-width="600px" v-model="dialog">
            <template v-slot:activator="{on,attrs}">
-            <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">New Item</v-btn>
+            <v-btn color="red lighten-2" v-bind="attrs" v-on="on">New Item</v-btn>
            </template>
-           <v-card class="pa-3 ma-3">
+           <v-card class="red lighten-3 dark">
                 <v-card-title class="pa-3 ma-3"><h2>Add new book</h2></v-card-title>
                 <v-text-field label="Book Name" v-model="newBook.name" class="pa-3 ma-3"> </v-text-field>
                 <v-text-field label="Author Name" v-model="newBook.author" class="pa-3 ma-3"> </v-text-field>
                 <v-text-field label="No. of Pages" v-model="newBook.pages" class="pa-3 ma-3"> </v-text-field>
-                <v-text-field label="Publishing Date" v-model="newBook.date" @click="datepicker=false" class="pa-3 ma-3">
-                    <!-- <template>
-                        <v-row justify="center">
-                            <v-date-picker
-                            v-model="date"
-                            :allowed-dates="allowedDates"
-                            class="mt-4"
-                            min="2016-06-15"
-                            max="2018-03-20"
-                            ></v-date-picker>
-                        </v-row>
-                    </template>    -->
-                </v-text-field>
+                <!-- <div class="mb-6">Active picker: </div> -->
+                <v-menu
+                    ref="menu"
+                    v-model="menu"
+                    :close-on-content-click="false"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="auto"
+                >
+                    <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                        v-model="date"
+                        label="Publishing date"
+                        prepend-icon="mdi-calendar"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                    ></v-text-field>
+                    </template>
+                    <v-date-picker
+                    v-model="date"
+                    min="1950-01-01"
+                    max="2022-11-30"
+                    @change="save"
+                    ></v-date-picker>
+                </v-menu>
                 <v-card-actions>
-                    
-                    
                     <!-- <v-btn color="primary" text @click="dialog=false" v-on:click="addItem">SAVE</v-btn> -->
-                    <v-btn color="primary" text  v-on:click="addItem">SAVE</v-btn>
+                    <v-btn color="blue"  v-on:click="addItem">SAVE</v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
@@ -44,20 +55,31 @@ export default {
                 name: '',
                 author: '',
                 pages:0,
-                date:'',
+                date:null,
            },
-           date: '2022-11-22',
-           dialog:false,
-        //    datepicker:false,
+           
+          dialog:false,
+          date: null,
+          menu: false,
+           
         }
     },
+    watch: {
+      menu (val) {
+        val && setTimeout(() => (this.activePicker = 'YEAR'))
+      },
+    },
     methods:{
-        addItem(){
-            this.$emit('addItem',this.newBook);
-        },
-        // allowedDates(val){
-        //     parseInt(val.split('-')[2], 10) % 2 === 0
-        // }
+      addItem(){
+        if(!this.newBook.name || !this.newBook.author || this.newBook === 0 || !this.newBook.date){
+            alert('Null entry cannot be added!')    
+        } else{
+          this.$emit('addItem',this.newBook);//validation block
+        }  
+      },
+      save (date) {
+        this.$refs.menu.save(date)
+      },
     }
   
 }
